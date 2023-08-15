383c6bfa04334f0a5661f576a97aa61bf38c22af
Karly Stark <karlystark@gmail.com>
2023-08-14 17:15:27 -0700

    WIP troubleshooting /registerbug

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,54 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const { BadRequestError, UnauthorizedError } = require("../expressError");
+const { SECRET_KEY } = require("../config");
+const { User } = require("../models/user");
+
 
 /** POST /login: {username, password} => {token} */
+router.post("/login", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
 
+  const { username, password } = req.body;
+
+  const authenticated = await User.authenticate(username, password);
+
+  if (authenticated) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+  throw new UnauthorizedError("Invalid credentials");
+
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const {
+    username,
+    password,
+    first_name,
+    last_name,
+    phone
+  } = req.body;
+
+  await User.register(
+    username,
+    password,
+    first_name,
+    last_name,
+    phone
+  );
+
+  const token = jwt.sign({ username }, SECRET_KEY);
+
+  return res.json({ token });
+});
+
 
 module.exports = router;
